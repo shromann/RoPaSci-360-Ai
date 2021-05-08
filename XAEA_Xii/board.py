3dbc1afea1953c837e5f6527d2092d11053a9a67
Shromann Majumder <shromannmajumder@Shromanns-MacBook-Pro.local>
2021-05-08 16:43:32 +1000

    curr2

--- XAEA_Xii/board.py
+++ XAEA_Xii/board.py
@@ -1,3 +1,24 @@
+<<<<<<< HEAD
+=======
+def play_rps(state_loc):
+    # Minimizez the hex at a particular location. The hex can have both upper and lowers tokens as well as its own type. 
+    
+    toks = ['r', 's', 'p']
+    to_remove = []
+    for t in range(3):
+        win = toks[t]
+        lose = toks[(t+1)%3]
+        if (win in state_loc and lose in state_loc) or (win.upper() in state_loc and lose in state_loc):
+            to_remove.append(lose)
+        if (win.upper() in state_loc and lose.upper() in state_loc) or (win in state_loc and lose.upper() in state_loc):
+            to_remove.append(lose.upper())
+        
+    for r in to_remove:
+        state_loc = list(filter(lambda t: t != r, state_loc))
+    return state_loc            
+
+
+>>>>>>> parent of bce3c3c... before the big change
 def update_player_state(state, action):
 
     mv_type = action[0]
@@ -6,6 +27,7 @@ def update_player_state(state, action):
     if mv_type == 'THROW':
         token = action[1]
     elif mv_type in ['SLIDE', 'SWING']:
+<<<<<<< HEAD
         if action[1] in state.keys():
             token = state[action[1]].pop()
             if not state[action[1]]:
@@ -14,11 +36,18 @@ def update_player_state(state, action):
             return state
 
     print(state, action)
+=======
+        token = state[action[1]].pop()
+        if  not state[action[1]]:
+            del state[action[1]]
+    
+>>>>>>> parent of bce3c3c... before the big change
     state[loc].append(token)
     state[loc] = play_rps(state[loc]) # minimize
 
     return state
 
+<<<<<<< HEAD
 def update_state(state, action):
 
     state['player'] = update_player_state(state['player'], action)
@@ -44,3 +73,11 @@ def play_rps(state_loc):
 
     return state_loc            
 
+=======
+
+# state = {"player":{}, "opponenet":{}}
+
+def update_state(state, action):
+    
+    ...
+>>>>>>> parent of bce3c3c... before the big change
